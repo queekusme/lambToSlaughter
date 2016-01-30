782029163ac1f58718bf78b77d3ba8751d0012b4
queekusme <queekusme@gmail.com>
2016-01-30 20:45:12 +0000

    oh my god, the music is terrible but works so well

--- LTS/Assets/Scripts/MovementController.cs
+++ LTS/Assets/Scripts/MovementController.cs
@@ -1,43 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MovementController : MonoBehaviour 
+public class MovementController : MonoBehaviour
 {
-	public float maxSpeed = 10.0f;
-	bool facingRight = true;
-
-	Animator anim;
 
 
-	// Use this for initialization
-	void Start () 
-	{
-		anim = GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () 
-	{
-		float move = Input.GetAxis ("Horizontal");
-		anim.SetFloat ("Speed", Mathf.Abs (move));
-		GetComponent<Rigidbody2D>().velocity = new Vector2 (move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+    public float maxSpeed = 10.0f;
+    private bool facingRight = true;
 
-        //flips
-		if (move > 0 && !facingRight)
+    private bool onGround; // jumping
+
+    // Use this for initialization
+    void Start() { }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        if ((facingRight && x < 0.0f) || (!facingRight && x > 0.0f)) { flip(); }
+
+        if (onGround && y > 0)
         {
-			FlipFacing ();
-		} else if (move < 0 && facingRight) 
-		{
-			FlipFacing ();
-		}
-	}
+            onGround = false;
+        }
 
-	void FlipFacing ()
-	{
-		facingRight = !facingRight;
-		Vector3 charScale = transform.localScale;
-		charScale.x *= -1;
-		transform.localScale = charScale;
-	}
+        GetComponent<Rigidbody2D>().velocity = new Vector2(x * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        Debug.Log(x);
+    }
 
+
+    void flip()
+    {
+        GetComponentInChildren<SpriteRenderer>().flipX = !GetComponentInChildren<SpriteRenderer>().flipX;
+        facingRight = !facingRight;
+    }
 }
